95156fdf4daa73e253a9621151b55241e79745bc
gkhntymz <gokhantaymaz@yahoo.com>
2026-01-19 13:23:53 +0300

    chore: clean up smoke-check usage in ViewController

--- EnterpriseApp/ViewController.swift
+++ EnterpriseApp/ViewController.swift
@@ -8,14 +8,19 @@
 import UIKit
 import CoreNetworking
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // Compile-time smoke-check
         _ = URLSessionHTTPClient()
+
+        // User-facing error mapping smoke-check
+        let sampleError = URLError(.notConnectedToInternet)
+        let appError = AppNetworkError.map(sampleError)
+
+        print("DEBUG ERROR:", sampleError)
+        print("USER MESSAGE:", appError.userMessage)
     }
-
-
 }
-
---
+++ EnterpriseAppTests/AppNetworkErrorMappingTests.swift
@@ -0,0 +1,7 @@
+//
+//  AppNetworkErorMappingTests.swift
+//  EnterpriseApp
+//
+//  Created by Gökhan Taymaz on 19.01.2026.
+//
+
